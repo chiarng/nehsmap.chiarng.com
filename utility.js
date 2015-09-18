776d47c4dc2076f32458716eb8c24b4d50ffb69f
Chiarng Lin <chiarng@gmail.com>
2015-09-18 10:51:11 -0700

    removed auth2 console log

--- utility.js
+++ utility.js
@@ -12,14 +12,7 @@ function addEvent(element, event, func) {
 
 // The Great Initializer
 function init() {
-	// auth2 is initialized with gapi.auth2.init() and a user is signed in.
-	if (auth2.isSignedIn.get()) {
-	  var profile = auth2.currentUser.get().getBasicProfile();
-	  console.log('ID: ' + profile.getId());
-	  console.log('Name: ' + profile.getName());
-	  console.log('Image URL: ' + profile.getImageUrl());
-	  console.log('Email: ' + profile.getEmail());
-	}
+
 };
 
 // window.onload
