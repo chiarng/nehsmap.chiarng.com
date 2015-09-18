c0cbf7162d0c73d4f8ca175709247b113e7b3581
Chiarng Lin <chiarng@gmail.com>
2015-09-18 03:27:04 -0700

    utility.js setup

---
+++ utility.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+// variable declarations
+
+// cross-browser support for attaching event listeners
+function addEvent(element, event, func) {
+	if (element.attachEvent)
+		return element.attachEvent('on' + event, func);
+	else
+		return element.addEventListener(event, func, false);
+};
+
+// The Great Initializer
+function init() {
+
+};
+
+// window.onload
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        init();
+        clearInterval(readyStateCheckInterval);
+    }
+}, 10);
