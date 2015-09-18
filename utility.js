fda3dca00e476e5a477347b9c03f79d71d73b1cb
Chiarng Lin <chiarng@gmail.com>
2015-09-18 11:11:24 -0700

    moved scripts to utility.js

--- utility.js
+++ utility.js
@@ -10,6 +10,26 @@ function addEvent(element, event, func) {
 		return element.addEventListener(event, func, false);
 };
 
+function onSignIn(googleUser) {
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	console.log("Name: " + profile.getName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail());
+
+	// The ID token you need to pass to your backend:
+	var id_token = googleUser.getAuthResponse().id_token;
+	console.log("ID Token: " + id_token);
+};
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      	console.log('User signed out.');
+    });
+};
+
 // The Great Initializer
 function init() {
 
